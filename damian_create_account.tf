0637c4839fd55e33023b83c564e6b1766953e671
Damian Cabrera <dcabrera@MacBook-Pro-de-Damian.local>
2025-03-14 15:41:47 -0300

    Nueva version

--- damian_create_account.tf
+++ damian_create_account.tf
@@ -1,4 +1,10 @@
 resource "aws_organizations_account" "account" {
   name  = "Damian Cabrera"
   email = "dcabrera+aws@bullkservices.uy"
+  parent_id = "536738705384"
+
+  tags = {
+	Environment = "External"
+	ManagedBy = "Terraform"
+}
 }
