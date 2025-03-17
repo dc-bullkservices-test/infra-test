d4c11273170772cbd33f6f79e6bac2bf8e21863d
ramirogrc <gsecco+damian@bullkservices.uy>
2025-03-17 14:22:22 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
-provider "aws" { 
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
- }
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your AWS region
+}
