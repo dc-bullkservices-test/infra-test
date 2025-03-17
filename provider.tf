9c8f702fbc5236599281df10038b31bfdbc99513
dc-bullkservices-test <dcabrera+test@bullkservices.uy>
2025-03-17 12:29:00 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 terraform {
-   provider "aws" = {
-     source  = "hashicorp/aws"
-     version = "~> 4.19.0"
+   required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.19.0"
+      }
    }
+}
+
+provider "aws" {
+   region = "us-east-1"
 }
