1889b701fe9e9a4a7100fc31648f2f67dec021de
dc-bullkservices-test <dcabrera+test@bullkservices.uy>
2025-03-17 15:14:55 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,19 +8,19 @@ terraform {
 }
 
 provider "aws" {
- access_key = var.access_key
- secret_key = var.secret_key
- region     = var.region
+ access_key = var.AWS_ACCESS_KEY_ID
+ secret_key = var.AWS_SECRET_ACCESS_KEY
+ region     = var.AWS_REGION
 }
 
-variable "region" {
+variable "AWS_REGION" {
    type = string
 }
 
-variable "access_key" {
+variable "AWS_ACCESS_KEY_ID" {
    type = string
 }
 
-variable "secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
    type = string
 }
